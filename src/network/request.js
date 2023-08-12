94cd5e2614ac50ac496ca83a1aca117736726621
luojie <rowe_jason@outlook.com>
2023-08-12 15:15:34 +0800

    home-init

--- src/network/request.js
+++ src/network/request.js
@@ -3,25 +3,24 @@ import axios from 'axios'
 export function request(config) {
   // 1.创建axios的实例
   const instance = axios.create({
-    baseURL: '123.207.32.32:7888/api/hy66',
+    baseURL: 'http://123.207.32.32:7888/api/hy66',
     timeout: 5000
   })
 
-  // 2.axios的拦截器
-  // 2.1.请求拦截的作用
+  // 2.请求拦截
   instance.interceptors.request.use(config => {
     return config
   }, err => {
     // console.log(err);
   })
 
-  // 2.2.响应拦截
+  // 3.响应拦截
   instance.interceptors.response.use(res => {
     return res.data
   }, err => {
     console.log(err);
   })
 
-  // 3.发送真正的网络请求
+  // 4.发送真正的网络请求
   return instance(config)
 }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 
 <script>
 
   import NavBar from "@/components/common/navbar/NavBar.vue";
+  import HomeSwiper from "@/views/home/childComps/HomeSwiper.vue";
+  import RecommendView from "@/views/home/childComps/RecommendView.vue";
 
+  import {getHomeMultidata} from "@/network/home";
   export default {
     name: "Home",
     components: {
-      NavBar
+      RecommendView,
+      HomeSwiper,
+      NavBar,
+
+    },
+    data(){
+      return{
+        banners: [],
+        recommends: []
+      }
+    },
+    /** 请求数据并保存 */
+    created() {
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
     }
   }
 </script>

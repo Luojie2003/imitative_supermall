48c7c20fcf55fe89d0c99ce8d8d3bdd984e66a63
luojie <rowe_jason@outlook.com>
2023-08-20 15:01:35 +0800

    detail init

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav"/>
+  </div>
+
+</template>
+
+<script>
+import DetailNavBar from "@/views/detail/childComps/DetailNavBar.vue";
+export default {
+  name: "Detail",
+  components: {
+    DetailNavBar
+  },
+  data() {
+    return {
+      iid: null
+    }
+  },
+  created() {
+    this.iid = this.$route.params.iid
+  }
+}
+</script>
+
+<style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+</style>
